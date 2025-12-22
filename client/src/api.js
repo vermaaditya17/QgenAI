dfc7c4cd809ed5bada4ce5ba833fd9cfab109e3e
vermaaditya17 <145982553+vermaaditya17@users.noreply.github.com>
2025-12-23 03:34:39 +0530

    Added the backend link to frontend

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
 // If we are live, use the backend URL. If local, use localhost.
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL || "https://qgen-backend1.onrender.com";
 
 export default API_URL;
